7ad0eb5c07f8aec1100c1c72d47c5b83477f4100
KiranSainikaur <kaurkirarpreet3@gmail.com>
2018-05-05 10:02:37 -0400

    Initial commit

---
+++ app/src/main/java/com/example/harjit/googlemap/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.harjit.googlemap;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+
+
+
+public class MainActivity extends AppCompatActivity  {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+}
